e1e72446e9cbf07ce7a5cf3d76fc20bfa7dac34d
Andrew Chou <andrewchou@fastmail.com>
2024-12-17 16:19:31 -0500

    chore: use explicit exports in entry file
    Offers more transparency and prevents accidental API exposure

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,69 @@
-export * from './contexts/ClientApi'
-export * from './hooks/client'
-export * from './hooks/documents'
-export * from './hooks/maps'
-export * from './hooks/projects'
-export * from './lib/react-query/client'
-export * from './lib/react-query/documents'
-export * from './lib/react-query/invites'
-export * from './lib/react-query/maps'
-export * from './lib/react-query/projects'
-export * from './lib/react-query/shared'
+export { ClientApiContext, ClientApiProvider } from './contexts/ClientApi'
+export {
+	useClientApi,
+	useIsArchiveDevice,
+	useOwnDeviceInfo,
+} from './hooks/client'
+export {
+	useManyDocs,
+	useSingleDocByDocId,
+	useSingleDocByVersionId,
+} from './hooks/documents'
+export { useMapStyleUrl } from './hooks/maps'
+export {
+	useAttachmentUrl,
+	useDocumentCreatedBy,
+	useIconUrl,
+	useManyMembers,
+	useManyProjects,
+	useProjectSettings,
+	useSingleMember,
+	useSingleProject,
+} from './hooks/projects'
+export {
+	deviceInfoQueryOptions,
+	getClientQueryKey,
+	getDeviceInfoQueryKey,
+	getIsArchiveDeviceQueryKey,
+	isArchiveDeviceQueryOptions,
+} from './lib/react-query/client'
+export {
+	documentByDocumentIdQueryOptions,
+	documentByVersionIdQueryOptions,
+	documentsQueryOptions,
+	getDocumentByDocIdQueryKey,
+	getDocumentByVersionIdQueryKey,
+	getDocumentsQueryKey,
+	getManyDocumentsQueryKey,
+	type DocumentType,
+} from './lib/react-query/documents'
+export {
+	getInvitesQueryKey,
+	getPendingInvitesQueryKey,
+	pendingInvitesQueryOptions,
+} from './lib/react-query/invites'
+export {
+	getMapsQueryKey,
+	getStyleJsonUrlQueryKey,
+	mapStyleJsonUrlQueryOptions,
+} from './lib/react-query/maps'
+export {
+	attachmentUrlQueryOptions,
+	documentCreatedByQueryOptions,
+	getAttachmentUrlQueryKey,
+	getDocumentCreatedByQueryKey,
+	getIconUrlQueryKey,
+	getMemberByIdQueryKey,
+	getMembersQueryKey,
+	getProjectByIdQueryKey,
+	getProjectRoleQueryKey,
+	getProjectSettingsQueryKey,
+	getProjectsQueryKey,
+	iconUrlQueryOptions,
+	projectByIdQueryOptions,
+	projectMembersQueryOptions,
+	projectOwnRoleQueryOptions,
+	projectSettingsQueryOptions,
+	projectsQueryOptions,
+} from './lib/react-query/projects'
+export { ROOT_QUERY_KEY, baseQueryOptions } from './lib/react-query/shared'
