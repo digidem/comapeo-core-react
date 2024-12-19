54f9bdcb53664fac820348050602b69007db7e18
Andrew Chou <andrewchou@fastmail.com>
2024-12-19 17:12:36 -0500

    implement useAcceptInviteMutation()

---
+++ src/hooks/invites.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { getInvitesQueryKey } from '../lib/react-query/invites.js'
+import { getProjectsQueryKey } from '../lib/react-query/projects.js'
+import { useClientApi } from './client.js'
+
+export function useAcceptInviteMutation() {
+	const queryClient = useQueryClient()
+	const clientApi = useClientApi()
+
+	const { mutate, status, reset } = useMutation({
+		mutationFn: async ({ inviteId }: { inviteId: string }) => {
+			return clientApi.invite.accept({ inviteId })
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: getInvitesQueryKey(),
+			})
+			queryClient.invalidateQueries({
+				queryKey: getProjectsQueryKey(),
+			})
+		},
+	})
+
+	return {
+		mutate,
+		reset,
+		status,
+	}
+}
