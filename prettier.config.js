7de0925507de8c15ceb4d45369ea39f2f4afbd96
Andrew Chou <andrewchou@fastmail.com>
2025-03-10 17:52:35 -0400

    chore(deps): update dev deps (#57)
    * update eslint deps
    
    * update typescript
    
    * update @types/node
    
    * update react-query deps
    
    * update vitest
    
    * revert change to tsconfig

--- prettier.config.js
+++ prettier.config.js
@@ -9,6 +9,6 @@ export default {
 	plugins: ['@ianvs/prettier-plugin-sort-imports'],
 	// Options for @ianvs/prettier-plugin-sort-imports
 	importOrder: ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '', '^[./]'],
-	importOrderTypeScriptVersion: '5.7.3',
+	importOrderTypeScriptVersion: '5.8.2',
 	importOrderCaseSensitive: false,
 }
