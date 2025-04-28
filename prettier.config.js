c70fec029fbdfdb318ce4436d9f39dcd81aa811e
Andrew Chou <andrewchou@fastmail.com>
2025-04-28 15:29:06 -0400

    chore(deps): update dev deps (#73)

--- prettier.config.js
+++ prettier.config.js
@@ -9,6 +9,6 @@ export default {
 	plugins: ['@ianvs/prettier-plugin-sort-imports'],
 	// Options for @ianvs/prettier-plugin-sort-imports
 	importOrder: ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '', '^[./]'],
-	importOrderTypeScriptVersion: '5.8.2',
+	importOrderTypeScriptVersion: '5.8.3',
 	importOrderCaseSensitive: false,
 }
