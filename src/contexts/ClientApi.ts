9494ceea32a6204fdd053d845c6bca355db7b488
Gregor MacLennan <gmaclennan@awana.digital>
2025-11-24 18:03:29 +0000

    chore!: Setup invite listeners automatically (#141)
    * chore!: Setup invite listeners automatically
    
    BREAKING CHANGE: Changes the type of the context value, and removes the public export of the Context, so that internal changes to the context value are not breaking changes in the future.
    
    * chore: deprecate useSetUpInvitesListeners()
    
    * update to always attach invite listeners
    
    * don't remove ClientContextApi

--- src/contexts/ClientApi.ts
+++ src/contexts/ClientApi.ts
@@ -1,11 +1,15 @@
 import type { MapeoClientApi } from '@comapeo/ipc' with { 'resolution-mode': 'import' }
+import { useQueryClient } from '@tanstack/react-query'
 import {
 	createContext,
 	createElement,
+	useEffect,
 	type Context,
 	type JSX,
 	type ReactNode,
 } from 'react'
+
+import { getInvitesQueryKey } from '../lib/react-query/invites.js'
 
 export const ClientApiContext: Context<MapeoClientApi | null> =
 	createContext<MapeoClientApi | null>(null)
@@ -24,6 +28,22 @@ export function ClientApiProvider({
 	children: ReactNode
 	clientApi: MapeoClientApi
 }): JSX.Element {
+	const queryClient = useQueryClient()
+
+	useEffect(() => {
+		function invalidateCache() {
+			queryClient.invalidateQueries({ queryKey: getInvitesQueryKey() })
+		}
+
+		clientApi.invite.addListener('invite-received', invalidateCache)
+		clientApi.invite.addListener('invite-updated', invalidateCache)
+
+		return () => {
+			clientApi.invite.removeListener('invite-received', invalidateCache)
+			clientApi.invite.removeListener('invite-updated', invalidateCache)
+		}
+	}, [clientApi, queryClient])
+
 	return createElement(
 		ClientApiContext.Provider,
 		{ value: clientApi },
