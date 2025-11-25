0825b6b193182a66ddcc48a965238371632c5b6c
Gregor MacLennan <gmaclennan@awana.digital>
2025-11-25 17:02:03 +0000

    feat: Add stubbed map sharing API

--- src/hooks/maps.ts
+++ src/hooks/maps.ts
@@ -1,7 +1,20 @@
-import { useSuspenseQuery } from '@tanstack/react-query'
+import {
+	useMutation,
+	useQueryClient,
+	useSuspenseQuery,
+} from '@tanstack/react-query'
 
-import { mapStyleJsonUrlQueryOptions } from '../lib/react-query/maps.js'
+import {
+	acceptMapShareMutationOptions,
+	getMapShareByIdQueryOptions,
+	getMapSharesQueryOptions,
+	mapStyleJsonUrlQueryOptions,
+	rejectMapShareMutationOptions,
+	requestCancelMapShareMutationOptions,
+	sendMapShareMutationOptions,
+} from '../lib/react-query/maps.js'
 import { useClientApi } from './client.js'
+import { useSingleProject } from './projects.js'
 
 /**
  * Get a URL that points to a StyleJSON resource served by the embedded HTTP server.
@@ -41,4 +54,119 @@ export function useMapStyleUrl({
 	)
 
 	return { data, error, isRefetching }
+}
+
+/**
+ * Get all map shares that the device has received.
+ *
+ * @example
+ * ```ts
+ * function Example() {
+ *   const { data } = useManyMapShares()
+ * }
+ * ```
+ */
+export function useManyMapShares() {
+	const clientApi = useClientApi()
+	const { data, error, isRefetching } = useSuspenseQuery(
+		getMapSharesQueryOptions({ clientApi }),
+	)
+
+	return { data, error, isRefetching }
+}
+
+/**
+ * Get a single map share based on its ID.
+ *
+ * @param opts.shareId ID of map share
+ *
+ * @example
+ * ```ts
+ * function Example() {
+ *   const { data } = useSingleMapShare({ shareId: '...' })
+ * }
+ * ```
+ */
+export function useSingleMapShare({ shareId }: { shareId: string }) {
+	const clientApi = useClientApi()
+
+	const { data, error, isRefetching } = useSuspenseQuery(
+		getMapShareByIdQueryOptions({ clientApi, shareId }),
+	)
+
+	return { data, error, isRefetching }
+}
+
+/**
+ * Accept and download a map share that has been received. The mutate promise
+ * resolves once the map _starts_ downloading, before it finishes downloading.
+ * The hooks useManyMapShares and useSingleMapShare can be used to track
+ * download progress.
+ */
+export function useAcceptMapShare() {
+	const queryClient = useQueryClient()
+	const clientApi = useClientApi()
+
+	const { error, mutate, mutateAsync, reset, status } = useMutation(
+		acceptMapShareMutationOptions({ clientApi, queryClient }),
+	)
+
+	return status === 'error'
+		? { error, mutate, mutateAsync, reset, status }
+		: { error: null, mutate, mutateAsync, reset, status }
+}
+
+/**
+ * Reject a map share that has been received.
+ */
+export function useRejectMapShare() {
+	const queryClient = useQueryClient()
+	const clientApi = useClientApi()
+
+	const { error, mutate, mutateAsync, reset, status } = useMutation(
+		rejectMapShareMutationOptions({ clientApi, queryClient }),
+	)
+
+	return status === 'error'
+		? { error, mutate, mutateAsync, reset, status }
+		: { error: null, mutate, mutateAsync, reset, status }
+}
+
+/**
+ * Share a map with a device. The mutation method resolves when the share is
+ * accepted (the recipient starts downloading the map), or if they reject the
+ * share or they already have that map on their device (this reply is
+ * automatic).
+ *
+ * @param opts.projectId Public ID of project to send the invite on behalf of.
+ */
+export function useSendMapShare({ projectId }: { projectId: string }) {
+	const queryClient = useQueryClient()
+	const { data: projectApi } = useSingleProject({ projectId })
+
+	const { error, mutate, mutateAsync, reset, status } = useMutation(
+		sendMapShareMutationOptions({ projectApi, projectId, queryClient }),
+	)
+
+	return status === 'error'
+		? { error, mutate, mutateAsync, reset, status }
+		: { error: null, mutate, mutateAsync, reset, status }
+}
+
+/**
+ * Request a cancellation of an invite sent to another device.
+ *
+ * @param opts.projectId Public ID of project to request the invite cancellation for.
+ */
+export function useRequestCancelInvite({ projectId }: { projectId: string }) {
+	const queryClient = useQueryClient()
+	const { data: projectApi } = useSingleProject({ projectId })
+
+	const { error, mutate, mutateAsync, reset, status } = useMutation(
+		requestCancelMapShareMutationOptions({ projectApi, queryClient }),
+	)
+
+	return status === 'error'
+		? { error, mutate, mutateAsync, reset, status }
+		: { error: null, mutate, mutateAsync, reset, status }
 }
